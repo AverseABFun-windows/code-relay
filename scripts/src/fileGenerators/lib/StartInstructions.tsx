6080d6ce3a5bdcec693056676eb9be668290e6b1
AverseABFun <81586710+AverseABFun-windows@users.noreply.github.com>
2023-01-02 13:26:52 -0600

    Update StartInstructions.tsx

--- scripts/src/fileGenerators/lib/StartInstructions.tsx
+++ scripts/src/fileGenerators/lib/StartInstructions.tsx
@@ -10,7 +10,7 @@ export default ({ divide }: { readonly divide?: boolean }) => (
       <li>
         Send an email to{" "}
         <a href={contributeMailHref}>{contributeEmailAddress}</a> or{" "}
-        <a href="https://github.com/mmulet/code-relay/issues">
+        <a href="https://github.com/AverseABFun-WIndows/code-relay/issues">
           open an issue code-relay repo
         </a>
         <br />
@@ -19,7 +19,7 @@ export default ({ divide }: { readonly divide?: boolean }) => (
         Body: Programming language/framework you want to work with
       </li>
       <li>
-        By 7 a.m. Pacific time the next day, Code Relay will reply with your
+        Within a day or two, Code Relay will reply with your
         first task! Spend 15 minutes or less on it, break it down into smaller
         tasks, etc. Then pass it on to the next contributor. All the fun of coding without the responsibility.
       </li>
