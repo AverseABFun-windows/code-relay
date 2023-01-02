386b1921512dd6e7561585ae20b17515a44aa846
AverseABFun <81586710+AverseABFun-windows@users.noreply.github.com>
2023-01-02 13:05:50 -0600

    Update FAQ.tsx

--- scripts/src/fileGenerators/lib/FAQ.tsx
+++ scripts/src/fileGenerators/lib/FAQ.tsx
@@ -34,9 +34,9 @@ export default () => (
         No. Code relay is meant to supplement current workflows, not replace.
       </QA>
       <QA q="What backend are you using? Is it open source, where is the repo?">
-        There isn't one, yet. Right now, I (<a href="https://github.com/mmulet">@mmulet</a>) respond to each email
+        There isn't one, yet. Right now, I (<a href="https://github.com/AverseABFun-Windows">@AverseABFun-Windows</a>) respond to each email
         personally. When code relay grows large enough that it needs an
-        automated backend, I intend to build it using code relay, and it will be
+        automated backend, mmulet(the actual creator of Code Relay) intend to build it using code relay, and it will be
         open source.
       </QA>
     </div>
